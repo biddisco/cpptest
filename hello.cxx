abf7ded1f06c9b9d28c191fe6ff8774d87c4bb8f
John Biddiscombe <biddisco@cscs.ch>
2014-05-21 07:27:11 +0200

    Clean up linking of tests and split into mpi/boost/etc sections

--- hello.cxx
+++ hello.cxx
@@ -1,11 +1,3 @@
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
 #include <iostream>
 
 int main(int argc, char **argv)
