9b8f4a5f12090bd979c97b86d682e663b785e2cf
John Biddiscombe <biddisco@cscs.ch>
2023-03-16 22:45:51 +0100

    Add mem usage check/test and cleanup cmake messages

---
+++ mem/mem_use.cxx
@@ -0,0 +1,89 @@
+#include <unistd.h>
+#include <ios>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <array>
+#include <vector>
+
+static const char *humanSize(uint64_t bytes)
+{
+	const char *suffix[] = {"B", "KB", "MB", "GB", "TB"};
+	char length = sizeof(suffix) / sizeof(suffix[0]);
+
+	int i = 0;
+	double dblBytes = bytes;
+
+	if (bytes > 1024) {
+		for (i = 0; (bytes / 1024) > 0 && i<length-1; i++, bytes /= 1024)
+			dblBytes = bytes / 1024.0;
+	}
+
+	static char output[200];
+	sprintf(output, "%.02lf %s", dblBytes, suffix[i]);
+	return output;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//
+// process_mem_usage(double &, double &) - takes two doubles by reference,
+// attempts to read the system-dependent data for a process' virtual memory
+// size and resident set size, and return the results in KB.
+//
+// On failure, returns 0.0, 0.0
+
+void process_mem_usage(double& vm_usage, double& resident_set)
+{
+   using std::ios_base;
+   using std::ifstream;
+   using std::string;
+
+   vm_usage     = 0.0;
+   resident_set = 0.0;
+
+   // 'file' stat seems to give the most reliable results
+   //
+   ifstream stat_stream("/proc/self/stat",ios_base::in);
+
+   // dummy vars for leading entries in stat that we don't care about
+   //
+   string pid, comm, state, ppid, pgrp, session, tty_nr;
+   string tpgid, flags, minflt, cminflt, majflt, cmajflt;
+   string utime, stime, cutime, cstime, priority, nice;
+   string O, itrealvalue, starttime;
+
+   // the two fields we want
+   //
+   unsigned long vsize;
+   long rss;
+
+   stat_stream >> pid >> comm >> state >> ppid >> pgrp >> session >> tty_nr
+               >> tpgid >> flags >> minflt >> cminflt >> majflt >> cmajflt
+               >> utime >> stime >> cutime >> cstime >> priority >> nice
+               >> O >> itrealvalue >> starttime >> vsize >> rss; // don't care about the rest
+
+   stat_stream.close();
+
+   long page_size_kb = sysconf(_SC_PAGE_SIZE) / 1024; // in case x86-64 is configured to use 2MB pages
+   vm_usage     = vsize / 1024.0;
+   resident_set = rss * page_size_kb;
+}
+
+int main()
+{
+   using std::cout;
+   using std::endl;
+
+   double total = 0;
+   std::array<std::vector<double>, 60> data;
+   for (int i=0; i<30; ++i) {
+       data[i] = std::move(std::vector<double>(1 << i, 0));
+       double vm, rss;
+       process_mem_usage(vm, rss);
+       total += data[i].size();
+       cout << "VM: " << humanSize(1024*vm) << "; RSS: " << humanSize(1024*rss) << " array " << humanSize(8*data[i].size()) << " total " << humanSize(total*8) << endl;
+   }
+
+  char c;
+  std::cin >> c;
+}
