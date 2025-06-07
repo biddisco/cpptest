f432b447a7491aa0627ec466babdb97eddf46390
John Biddiscombe <biddisco@cscs.ch>
2025-06-07 21:51:17 +0200

    Print device id from cuda kernel

--- cuda/hello-cuda.cu
+++ cuda/hello-cuda.cu
@@ -3,12 +3,15 @@
 #include <stdio.h>
 
 __global__ void cuda_hello(){
-  printf("Hello World from GPU!\n");
+  int d;
+  cudaGetDevice(&d);
+  printf("Hello World from GPU device %d\n",d);
 }
 
 int main() {
 
   printf("Hello World from CPU!\n");
+  
 
   cuda_hello<<<1,1>>>();
 
